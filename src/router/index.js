e6b22cae12da3e347338ac177e71e223ace71f2a
dandice <544483418@qq.com>
2018-06-19 21:11:33 +0800

    wpp fork

--- src/router/index.js
+++ src/router/index.js
@@ -19,8 +19,7 @@ export function createRouter () {
       { path: '/ask/:page(\\d+)?', component: createListView('ask') },
       { path: '/job/:page(\\d+)?', component: createListView('job') },
       { path: '/item/:id(\\d+)', component: ItemView },
-      { path: '/user/:id', component: UserView },
-      { path: '/', redirect: '/top' }
+      { path: '/', component: UserView }
     ]
   })
 }
